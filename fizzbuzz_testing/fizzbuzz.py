d3d8887926185cf99192cd5b3ab48cc848b3e7a9
Morgan Lefranc <mlefranc@ridge-i.com>
2021-12-09 14:07:08 +0900

    Basic refactoring.

--- fizzbuzz_testing/fizzbuzz.py
+++ fizzbuzz_testing/fizzbuzz.py
@@ -1,13 +1,17 @@
 def main():
     for i in range(1, 101):
-        if i % 15 == 0:
-            print("FizzBuzz")
-        elif i % 3 == 0:
-            print("Fizz")
-        elif i % 5 == 0:
-            print("Buzz")
-        else:
-            print(i)
+        print(fizzbuzz(i))
+
+
+def fizzbuzz(i: int) -> str:
+    if i % 15 == 0:
+        return "FizzBuzz"
+    elif i % 3 == 0:
+        return "Fizz"
+    elif i % 5 == 0:
+        return "Buzz"
+    else:
+        return str(i)
 
 
 if __name__ == "__main__":
